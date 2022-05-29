03313752cbe16778a83f9b14c23782a3529b9816
owuordove@gmail.com <owuordove@gmail.com>
2022-05-29 15:38:32 +0300

    shell.h file

---
+++ shell.h
@@ -0,0 +1,12 @@
+/*
+ * All rights reserved.
+ */
+#ifndef MYSHELL_H_
+#define MYSHELL_H_
+
+// Include your relevant functions declarations here they must start with the 
+// extern keyword such as in the following example:
+// extern void display_help(void);
+
+
+#endif /* MYSHELL_H_ */
